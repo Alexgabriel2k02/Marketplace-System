88d75b8319ed6decf2c5f66e330ecd65e9a6eabc
DevGuixavier <dev.guixavier@gmail.com>
2025-03-26 01:24:34 -0300

    update data_base.py
    reajuste da criação de banco de dados

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -9,12 +9,14 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market_management.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# Inicialize o SQLAlchemy sem passar o app diretamente
+db = SQLAlchemy()
 
 def init_db(app):
     """
     Inicializa a base de dados com o app Flask e o SQLAlchemy.
     """
-    # Remova a chamada redundante db.init_app(app)
-    pass
+    db.init_app(app)  # Associa o banco de dados ao app Flask
+    with app.app_context():
+        db.create_all()  # Cria as tabelas no banco de dados
 
