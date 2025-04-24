1283dad96d6c1875212e00f5a3dfeed9f82ef27a
DevGuixavier <dev.guixavier@gmail.com>
2025-04-24 16:38:04 -0300

    Create seller.py
    feat: adiciona model Seller com campos de autenticacao e status

---
+++ src/Infrastructure/Model/seller.py
@@ -0,0 +1,25 @@
+from src.config.data_base import db
+
+
+class Seller(db.Model):
+    __tablename__ = "sellers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    cnpj = db.Column(db.String(18), unique=True, nullable=True)
+    phone = db.Column(db.String(15),unique=True, nullable=True)
+    status = db.Column(db.String(10), nullable=True, default="Inativo")
+    verification_code = db.Column(db.String(4), nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "cnpj": self.cnpj,
+            "phone": self.phone,
+            "status": self.status,
+            "verification_code": self.verification_code,
+        }
