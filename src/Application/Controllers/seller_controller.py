09fec239ce48163c0c527427b63fa000cbac5a77
DevGuixavier <dev.guixavier@gmail.com>
2025-04-24 13:46:09 -0300

    feat: Implementa SellerController com metodos de autenticacao e cadastro
    Adiciona a classe SellerController contendo os metodos
    para criar (create_seller), ativar (activate_seller) e realizar login (login) de vendedores.
    
    Inclui a logica para tratar os diferentes tipos de retorno
    da funcao activate_seller no SellerService (sucesso ou erro).
    
    Estes metodos recebem as requisicoes HTTP relacionadas a vendedores,
    interagem com o SellerService e retornam as respostas formatadas.

---
+++ src/Application/Controllers/seller_controller.py
@@ -0,0 +1,39 @@
+from flask import request, jsonify, make_response
+from flask_jwt_extended import create_access_token
+from src.Application.Service.seller_service import SellerService
+from src.Infrastructure.Model.seller import Seller
+
+
+class SellerController:
+    @staticmethod
+    def create_seller():
+        body = request.get_json()
+        result, status_code = SellerService.create_seller(body)
+        return jsonify(result), status_code
+
+    @staticmethod
+    def activate_seller():
+        body = request.get_json()
+        result = SellerService.activate_seller(body)
+
+        # Verifica se o resultado é uma tupla (erro) ou um dicionário (sucesso)
+        if isinstance(result, tuple):
+            message, status_code = result  # Desempacota a tupla
+            return jsonify(message), status_code
+        else:
+            # Se for um dicionário (sucesso), retorna a mensagem e o código 200
+            return jsonify(result), 200
+
+    @staticmethod
+    def login(email, password):
+        # Verifique as credenciais do usuário
+        user = Seller.query.filter_by(email=email).first()
+        if user and user.password == password:  # Em produção, use hashing para senhas
+            # Use o e-mail como identity (string) e adicione claims extras, se necessário
+            access_token = create_access_token(
+                identity=email,  # O identity agora é uma string
+                additional_claims={"name": user.name, "status": user.status},
+            )
+            return jsonify(access_token=access_token), 200
+
+        return jsonify({"mensagem": "Credenciais inválidas"}), 401
