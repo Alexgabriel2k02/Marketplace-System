0453242a45653e0b4d0fa59e8408ededa91fe33d
DevGuixavier <dev.guixavier@gmail.com>
2025-04-24 13:29:10 -0300

    feat: Adiciona configuracao inicial em run.py
    adiciona a configuracao basica do Flask, inicializa as rotas
    usando a funcao init_routes e configura a conexao com o banco de dados.
    Isso estabelece a estrutura minima necessaria para rodar a aplicacao.

--- run.py
+++ run.py
@@ -2,12 +2,26 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from src.routes import init_routes
 from src.config.data_base import app, init_db, db
+from src.Infrastructure.Model.seller import Seller
+from src.Infrastructure.Model.product import Product
+from src.Infrastructure.Model.sales import Sale
+from src.Infrastructure.Model.orders import Order
 
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Substitua por uma chave secreta segura
+# Configuração do JWT
+app.config["JWT_SECRET_KEY"] = (
+    "your_jwt_secret_key"  # Substituir por uma chave secreta segura
+)
 jwt = JWTManager(app)
 
+# Inicializa o banco de dados
+init_db(app)
+
+# Cria as tabelas no banco de dados
+with app.app_context():
+    db.create_all()
+
+# Inicializa as rotas
 init_routes(app)
 
-if __name__ == '__main__':
-    init_db(app)  # Passa o app para inicializar o banco de dados
-    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
