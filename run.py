f921693b58e637fc411624aee29bae03100d0152
DevGuixavier <dev.guixavier@gmail.com>
2025-04-25 00:04:48 -0300

    feat: configurar expiração do token JWT para 1 hora no run.py
    Adiciona configuração no run.py para aumentar o tempo de expiração do token JWT para 1 hora, melhorando a experiência do usuário ao reduzir a frequência de logins necessários.

--- run.py
+++ run.py
@@ -1,20 +1,24 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS  
+from flask_cors import CORS
 from src.routes import init_routes
 from src.config.data_base import app, init_db, db
 from src.Infrastructure.Model.seller import Seller
 from src.Infrastructure.Model.product import Product
 from src.Infrastructure.Model.sales import Sale
 from src.Infrastructure.Model.orders import Order
-
+from datetime import timedelta
 
 # Quando o frontend React estiver pronto, substituir por:
-# CORS(app, resources={r"/*": {"origins": "http://localhost:porta"}}) 
-CORS(app)  
+# CORS(app, resources={r"/*": {"origins": "http://localhost:porta"}})
+CORS(app)
 
 
-app.config["JWT_SECRET_KEY"] = "your_jwt_secret_key"  # Substituir por uma chave secreta segura
+app.config["JWT_SECRET_KEY"] = (
+    "your_jwt_secret_key"  # Substituir por uma chave secreta segura
+)
+
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(app)
 
 init_db(app)
