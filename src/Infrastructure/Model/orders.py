b7cc33f66e69a753a827d0a623c935e516135be5
DevGuixavier <dev.guixavier@gmail.com>
2025-04-27 21:18:33 -0300

    refactor: alterar model Order para usar client_id e relacionament
    Remove campo client_name, adiciona client_id como ForeignKey para clients.id e ajusta retorno do método to_dict.

--- src/Infrastructure/Model/orders.py
+++ src/Infrastructure/Model/orders.py
@@ -1,18 +1,21 @@
 from src.config.data_base import db
+from src.Infrastructure.Model.client import Client
 
 
 class Order(db.Model):
     __tablename__ = "orders"
 
     id = db.Column(db.Integer, primary_key=True)
-    client_name = db.Column(db.String(100), nullable=False)  # Nome do cliente
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'), nullable=False)
     product_name = db.Column(db.String(100), nullable=False)  # Nome do produto
     quantity = db.Column(db.Integer, nullable=False)  # Quantidade do produto
+
+    client = db.relationship('Client', backref=db.backref('orders', lazy=True))
 
     def to_dict(self):
         return {
             "id": self.id,
-            "client_name": self.client_name,
+            "client": self.client.to_dict() if self.client else None,
             "product_name": self.product_name,
             "quantity": self.quantity,
         }
