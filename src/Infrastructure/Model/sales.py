ff3aa3cbcd30e08d000c13f4dee5efaae87208a6
DevGuixavier <dev.guixavier@gmail.com>
2025-06-10 14:01:36 -0300

    feat: add order_id e no to_dict de sales
    Inclui o campo order_id no modelo Sale e no método to_dict, permitindo associar vendas a pedidos.

--- src/Infrastructure/Model/sales.py
+++ src/Infrastructure/Model/sales.py
@@ -7,6 +7,7 @@ class Sale(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
     seller_id = db.Column(db.Integer, db.ForeignKey("sellers.id"), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), nullable=True)
     quantity = db.Column(db.Integer, nullable=False)
     unit_price = db.Column(db.Float, nullable=False)  
     total_price = db.Column(db.Float, nullable=False)  
@@ -17,6 +18,7 @@ class Sale(db.Model):
             "id": self.id,
             "product_id": self.product_id,
             "seller_id": self.seller_id,
+            "order_id" : self.order_id,
             "quantity": self.quantity,
             "unit_price": self.unit_price,  
             "total_price": self.total_price,
